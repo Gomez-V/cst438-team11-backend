302f1a34585130dc41c6476b3af087b7cf9707e6
Gomez-V <160459958+Gomez-V@users.noreply.github.com>
2025-04-13 19:25:09 -0700

    Update StudentController.java

--- cst438-team11-backend/src/main/java/com/cst438/controller/StudentController.java
+++ cst438-team11-backend/src/main/java/com/cst438/controller/StudentController.java
@@ -65,38 +65,5 @@ public class StudentController {
         return dtoList;
    }
 
-    /**
-     students lists their assignments given year and semester value
-     returns list of assignments may be empty
-     logged in user must be the student (assignment 7)
-     */
-    @GetMapping("/assignments")
-    public List<AssignmentStudentDTO> getStudentAssignments(
-            @RequestParam("studentId") int studentId,
-            @RequestParam("year") int year,
-            @RequestParam("semester") String semester) {
-
-        List<Assignment> assignments = assignmentRepository.findByStudentIdAndYearAndSemesterOrderByDueDate(studentId, year, semester);
-        List<AssignmentStudentDTO> dtoList = new ArrayList<>();
-        for (Assignment a : assignments) {
-            Enrollment enrollment = enrollmentRepository.findEnrollmentBySectionNoAndStudentId(a.getSection().getSectionNo(), studentId);
-            Grade grade = null;
-            if (enrollment != null) {
-                grade = gradeRepository.findByEnrollmentIdAndAssignmentId(
-                      enrollment.getEnrollmentId(),
-                      a.getAssignmentId());
-            }
-            Integer score = grade != null ? grade.getScore() : null;
-            dtoList.add(new AssignmentStudentDTO(
-                    a.getAssignmentId(),
-                    a.getTitle(),
-                    a.getDueDate(),
-                    a.getSection().getCourse().getCourseId(),
-                    a.getSection().getSecId(),
-                    score
-            ));
-        }
-        return dtoList;
-    }
 
 }
